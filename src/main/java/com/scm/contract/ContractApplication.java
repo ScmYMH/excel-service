7b29a88852b40435b60b4add547e0aa37dd1f24d
hyeja <21800799@handong.edu>
2022-08-30 10:26:37 +0900

    feat main

--- src/main/java/com/scm/contract/ContractApplication.java
+++ src/main/java/com/scm/contract/ContractApplication.java
@@ -2,8 +2,10 @@ package com.scm.contract;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class ContractApplication {
 
 	public static void main(String[] args) {
